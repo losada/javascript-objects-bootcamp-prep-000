a26df0699abd47bd0ee9446ed3049c019be6e080
Michael Losada <michael.losada@gmail.com>
2018-02-18 21:58:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,8 @@ var playlist = {
 function updatePlaylist(playlist, artist, songTitle){
   return Object.assign({}, playlist, {[artist]: songTitle})
 }
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
+}
